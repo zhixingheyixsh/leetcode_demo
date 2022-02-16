fdde5823d9e3a2bc5463ebb2e69a2acb0ba87d4a
xish <zhixingheyixsh@163.com>
2022-02-16 16:03:38 +0800

    dp && center extend

--- longestPalindrome.cc
+++ longestPalindrome.cc
@@ -18,16 +18,6 @@ out_type convert(const input_type &t) {
     return result;
 }
 
-bool diff_two_vector(std::vector<int> x,std::vector<int> y) {
-    if (x.size() != y.size()) {
-        return false;
-    }
-    for (int i = 0; i < x.size(); ++i) {
-        if (x[i] != y[i])   return false;
-    }
-    return true;
-}
-
 std::string preprocess(const std::string str) {
     std::string res;
     for (auto &k : str) {
@@ -40,45 +30,76 @@ std::string preprocess(const std::string str) {
 
 //resolution1: dynamic programing
 std::string  longPalindrome_dp(const std::string str) {
-    std::string data = preprocess(str);
     int i, j;
-    std::vector<std::vector<int>> dp(str.size(), 0);
-    for ( auto &t: dp) {
-        t.push_back(std::vector<int>(str.size()));
-    }
-    //init
-    for (i = 0; i < str.size(); i++) {
+    std::vector<std::vector<int>> dp(str.size(), std::vector<int>(str.size(), 0));
+    //init boundary
+    for (i = 0; i < str.size() - 1; i++) {
         dp[i][i] = 1;
+        dp[i][i+1] = (str[i]==str[i+1]);
     }
-    for (i = 1; i < str.size(); i++) {
-        if (dp[0][i-1] == 1) {
-        } else {
-
+    int max_len = 0;
+    int begin = 0, end = 0;
+    for (i = str.size() - 2; i >= 0; i--) {
+        for (j = i + 2; j <  str.size(); j++) {
+            if (dp[i+1][j-1] == 1) {
+                if (str[i] == str[j]) {
+                    dp[i][j] = 1;
+                    int temp = j - i + 1;
+                    if (max_len < temp) {
+                        max_len = temp;
+                        begin = i;
+                        end = j;
+                    }
+                }
+            } else {
+                dp[i][j] = 0;
+            }
         }
     }
-
-    for (i = str.size() -1 ; i > 0; i--) {
-
-    }
-    // i < j
-    for (i = 0; i < str.size(); ++i) {
-        for (j = i + 1; j < str.size(); ++j) {
-            if (dp[][])
-        }
-    }
-    return str.substring();
+    return str.substr(begin, end - begin + 1);
 }
+
+std::string longPalindrome_center_extend(const std::string str) {
+    std::string data = preprocess(str);
+    int len = data.length();
+    int i, j;
+    int center_pos = 0;
+    int round = 0;
+    std::string res;
+    for (i = 0; i < len; ++i) {
+        //center pivilot char
+        j = 1;
+        while (i + j < len && i - j >= 0) {
+            if (data[i+j]!=data[i-j]) {
+                break;
+            }
+            j++;
+        }
+        if (round < j) {
+            round = j;
+            center_pos = i;
+        }
+    }
+    // !!! round is one more than the real longth
+    return str.substr((center_pos - round  + 1)/2, round - 1);
+}
+
+
 int main(int argc, char* argv[]){
     std::vector<std::string> vec_str;
     unsigned int ind = 0;
-    vec_str.emplace_back("aaaaaab");
+    vec_str.emplace_back("ab");
     vec_str.emplace_back("fabcddcbae");
     vec_str.emplace_back("mmsgbg");
     vec_str.emplace_back("gabaf");
-    std::cout << "input vector<string> is " << std::endl;
+    vec_str.emplace_back("f");
     for (auto &k : vec_str){        
-        std::cout << preprocess(k) << std::endl;
+        std::cout << "input: " << k << std::endl;
+        std::cout << "[dp] out: " << longPalindrome_dp(k) << std::endl;
+        //std::cout << preprocess(k) << std::endl;
+        std::cout << "[center_extend] out: " << longPalindrome_center_extend(k) << std::endl;
     }
+    std::cout << "----------------" << std::endl;
     return 0;
 }
 
